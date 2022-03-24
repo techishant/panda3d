6545700a215175eea30127abffe82c2dcdcfd61c
techishant <ishantsh12234@gmail.com>
2022-03-24 12:15:45 +0530

    update

--- 02-.py
+++ 02-.py
@@ -1,9 +1,11 @@
 from direct.showbase.ShowBase import ShowBase
 from panda3d.core import WindowProperties
+# from panda3d.core import loader
 
 class App(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
+        loader.loadModel("Models/Misc/environment")
 
         properties = WindowProperties()
         properties.setSize(1000,756)
